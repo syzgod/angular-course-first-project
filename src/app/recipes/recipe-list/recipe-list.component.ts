c05bb6fd990d138e99a671e4e69600be6d86ce94
syzgod <viktor.berczeli@gmail.com>
2023-12-30 00:40:21 +0100

    Add Bootstrap library and update app structure

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css',
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvYTAxOS1qYWt1YmstMDU3NS1jaGlja2VuLWxlZ3MtdG9tYXRvZXMtcGVwcGVycy1vcmFuZ2VzMi5qcGc.jpg'
+    ),
+  ];
+
+  constructor() {}
+}
